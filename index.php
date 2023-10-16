40b0021c3df1cd59d62169cf8a927a25cd932501
Max12243 <laurin.maxime1@gmail.com>
2023-10-16 10:40:11 -0400

    Copie de apical

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Votre site web</title>
+    <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+</head>
+
+
+<header>
+    <div class="container1">
+        <div class="image-container">
+            <img src="media/bg.jpg" alt="Apical, ma plateforme d'apprentissage">
+        </div>
+        <div class="icon-container">
+            <!-- Placez ici vos icônes -->
+            <i class="fa fa-icon1"></i>
+            <i class="fa fa-icon2"></i>
+            <i class="fa fa-icon3"></i>
+        </div>
+    </div>
+
+
+
+</header>
+
+
+<div class="sidebar">
+    <ul class="horizontal-list">
+        <li><a>FORMATIONS</a></li>
+        <li><a>BLOGUE</a></li>
+        <li><a>OUTILS</a></li>
+        <li><a>AIDE</a></li>
+    </ul>
+
+</div>
+
+
+<body>
+    <div class="content">
+        
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
+    </div>
+</body>
+
+
+<footer>
+    <div class="container">
+        <div class="footer-item">
+            <p>© 2023 Tous droits réservés.</p>
+        </div>
+        <div class="footer-item">
+            <p>Conception et développement de la plateforme : Maxime Laurin(c'est faux, site plagié de Christiane Lagacé)</p>
+        </div>
+        <div class="footer-item">
+            <p><button onclick="window.location.href='https://www.google.com'">Écrivez-moi !</button></p>
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+</html>
