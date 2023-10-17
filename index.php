3de3ccf24708ee631437e7b10544fd949fed5b74
Max12243 <laurin.maxime1@gmail.com>
2023-10-17 10:18:37 -0400

    2e commit

--- index.php
+++ index.php
@@ -1,75 +1,268 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr-CA">
+
+
+<!-- Mirrored from apical.xyz/formations/wordpress by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 09 Oct 2023 20:47:34 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head>
-    <title>Votre site web</title>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5HQFPLZYQK"></script>
+
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Contenu de la formation  PUB020&amp;nbsp;: WordPress sur Apical." />
+    <meta name="author" content="Christiane Lagacé : https://christianelagace.com">
+    
+    <meta name="csrf-token" id="csrf-token" content="uzdpbgMctuI7uF4ieyWMTXrX8CuXoGiKvBdeuUSN">
+
+    <title>Apical | Formation PUB020 : WordPress</title>
     <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+
+    
+    
+             
+        
+                    <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
+            <link rel="manifest" href="../site.html">
+            <link rel="mask-icon" href="../safari-pinned-tab.svg" color="#3543a0">
+            <meta name="msapplication-TileColor" content="#2b5797">
+            <meta name="theme-color" content="#ffffff">
+            
+    <meta name="robots" content="noodp" />
+
+    <script data-ad-client="ca-pub-0968524214519104" async src="../../pagead2.googlesyndication.com/pagead/js/f.txt"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+    
+    <link href="https://fonts.googleapis.com/css?family=Homemade+Apple" rel="stylesheet">
+
+    
+    
 </head>
 
 
-<header>
-    <div class="container1">
-        <div class="image-container">
-            <img src="media/bg.jpg" alt="Apical, ma plateforme d'apprentissage">
-        </div>
-        <div class="icon-container">
-            <!-- Placez ici vos icônes -->
-            <i class="fa fa-icon1"></i>
-            <i class="fa fa-icon2"></i>
-            <i class="fa fa-icon3"></i>
+
+
+<header id="messtyles">
+    <div class="aucunepubauto">
+        <div class="logo fix-menu" id="zoneentete">
+            <div class="container">
+                <div class="avant"></div>
+                    <div class="apres">
+                        <div id="menuicones" class="porteuroffset fix-menu" data-verticaloffset="47" data-horizontaloffset="0">
+                            <a href="../index.html"><img src="https://apical.xyz/medias/commun/Accueil-MenuSecondaire.svg" alt="Accueil" title="Accueil"></a>
+                            <a href="#" class="ouvrirpopupchristiane ouvrirpopuprecherche" data-target="#popuprecherche"><img src="https://apical.xyz/medias/commun/Rechercher-MenuSecondaire.svg" alt="Recherche" title="Rechercher"></a>
+                            <a href="#" class="ouvrirpopupchristiane ouvrirpopupauthentification" data-target="#popupauthentification"><img src="https://apical.xyz/medias/commun/Login-MenuSecondaire.svg" alt="Authentification" title></a>
+                        </div>
+                    </div>
+                    <div class="centre">
+                        <img src="https://apical.xyz/medias/fr/LogoApical-blanc.svg" alt="Apical, ma plateforme d'apprentissage">
+                    </div>
+                </div>
+            </div>
+
+        
+            <nav class="navbar navbar-expand-lg navbar-light py-lg-3" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Apical</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                        aria-controls="navbarResponsive" aria-expanded="false" aria-label="Basculer">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+
+            <ul class="navbar-nav mx-auto">
+
+                
+
+                <li class="nav-item px-lg-4 ">
+                    <a href="../formations.html"
+                       class=" nav-link text-uppercase text-expanded active"
+                    >Formations <span class="sr-only">(actuel)</span></a>
+                    </li>
+
+                
+
+                
+                
+
+                
+                
+
+                <li class="nav-item px-lg-4">
+                    
+                    <a href="../articles.html"
+                       class="nav-link text-uppercase text-expanded ">Blogue </a>
+                </li>
+
+                
+                <li class="nav-item px-lg-4 dropdown">
+                    <a href="../pages/formulairebcrypt.html"
+                       class="dropdown-toggle nav-link text-uppercase text-expanded  "
+                            data-toggle="dropdown" data-target="#" aria-haspopup="true" aria-expanded="false">Outils </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item "
+                               href="../pages/formulairebcrypt.html">Hachage bcrypt </a>
+                            <a class="dropdown-item "
+                               href="../pages/aleatoire.html">Générateur aléatoire </a>
+                            <a class="dropdown-item "
+                               href="../pages/fontAwesome.html">Icônes Font Awesome </a>
+                        </div>
+                </li>
+
+                
+
+                
+                
+
+                <li class="nav-item px-lg-4 dropdown">
+                    <a href="../contact.html"
+                       class="dropdown-toggle nav-link text-uppercase text-expanded "
+                        data-toggle="dropdown" data-target="#" aria-haspopup="true" aria-expanded="false">Aide </a>
+                    <div class="dropdown-menu">
+                        
+                        <a class="dropdown-item "
+                           href="../contact.html">Contact </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item "
+                           href="../apropos.html">À
+                            propos </a>
+                    </div>
+                </li>
+
+            </ul>
+
+            
+            <div class="iconespourmobile">
+                <div id="menuicones" class="porteuroffset fix-menu" data-verticaloffset="47" data-horizontaloffset="0">
+    <a href="../index.html"><img src="../medias/commun/Accueil-MenuSecondaire.svg" alt="Accueil" title="Accueil" /></a>
+
+    
+    
+    <a href="#" class="ouvrirpopupchristiane ouvrirpopuprecherche" data-target="#popuprecherche"><img src="../medias/commun/Rechercher-MenuSecondaire.svg" alt="Recherche" title="Rechercher"/></a>
+    <a href="#" class="ouvrirpopupchristiane ouvrirpopupauthentification" data-target="#popupauthentification"><img src="../medias/commun/Login-MenuSecondaire.svg" alt="Authentification" title="" /></a>
+</div>
+            </div>
+
         </div>
     </div>
-
-
-
-</header>
-
-
-<div class="sidebar">
-    <ul class="horizontal-list">
-        <li><a>FORMATIONS</a></li>
-        <li><a>BLOGUE</a></li>
-        <li><a>OUTILS</a></li>
-        <li><a>AIDE</a></li>
-    </ul>
-
-</div>
-
+</nav>
+                               
+        
+        
+            </div>
+    </header>
 
 <body>
-    <div class="content">
-        
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-    </div>
-</body>
-
-
-<footer>
     <div class="container">
-        <div class="footer-item">
-            <p>© 2023 Tous droits réservés.</p>
+
+        
+        <div id="listefavoris">
+                    </div>
+
+        
+        <div id="dernierespagesvisitees">
+                    </div>
+
+        <div class="contenuprincipal">
+                        <hr class="divider">
+            <h1 id="titreh1" class="text-center">    Formation PUB020&nbsp;: WordPress, 2023
+</h1>
+            <hr class="divider bas">
+
+                                    <div class="contenu">
+                    <div id="chapitresformation" data-formation-id="36">
+
+        
+                    <div class="boutonshaut">
+                <div class="float-left">
+                                    </div>
+                <div class="float-right">
+                    <a id="developperreduire" href="#" class="btn btn-secondary" role="button" data-developper="Tout développer" data-reduire="Tout réduire">Tout développer</a>
+                </div>
+                <div class="push"></div>
+            </div>
+            <div id="dragchapitres">
+                                                            
+                        
+                        <div class="card border-bottom-0" id="dragchapitre_2990">
+                                                            
+                                <div class="card-header" id="chapitre-choisir_les_outils_pour_developper_un_site_wordpress">
+                                    <a data-toggle="collapse" href="#fichesduchapitre-choisir_les_outils_pour_developper_un_site_wordpress">
+                                        <span class="titrealigneboutons">1. Choisir les outils pour développer un site WordPress</span>
+                                    </a>
+                                    <div class="float-right boutonsalignes">
+                                    </div>
+                                </div>
+                                <div class="collapse" aria-expanded="false" id="fichesduchapitre-choisir_les_outils_pour_developper_un_site_wordpress">
+                                    <div class="card-body aucune-marge-haut-bas listefichesajax" id="fichesduchapitre-2990" data-id="2990">
+                                        <span class="encoursdegeneration">Liste en cours de génération...</span>
+                                    </div>
+                                </div>
+                        </div>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                                                                                
+             
+                   
+                        
+
+        <div class="boutonsbas">
+                    </div>
+    </div>
+
+            </div>
+            
+            <div class="push"></div>
         </div>
-        <div class="footer-item">
-            <p>Conception et développement de la plateforme : Maxime Laurin(c'est faux, site plagié de Christiane Lagacé)</p>
-        </div>
-        <div class="footer-item">
-            <p><button onclick="window.location.href='https://www.google.com'">Écrivez-moi !</button></p>
+
+        <div class="publicite baspage">
+            <div class="pubhebergement">
+                <p>Site fièrement hébergé chez <a href="http://www.a2hosting.com/?aid=5ca65a17be949" target="_top">A2 Hosting</a>.</p>
+                <p><a href="https://www.a2hosting.com/?aid=5ca65a17be949&amp;bid=ed1c4a67" target="_top"><img src="../../affiliates.a2hosting.com/accounts/default1/banners/ed1c4a67.jpg" alt="" title="" width="728" height="90" /></a><img style="border:0" src="https://affiliates.a2hosting.com/scripts/imp.php?aid=5ca65a17be949&amp;bid=ed1c4a67" width="1" height="1" alt="" /></p>
+            </div>
         </div>
     </div>
-</footer>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">© 2023 Tous droits réservés.</div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">Conception et développement de la plateforme : <span class="auteur"><a href="https://christianelagace.com/" target="_blank">Christiane Lagacé</a></span></div>
+                <div class="col text-center">En vue du Cours Dev Web 2, recopiage du site : <span class="auteur"><a href="https://christianelagace.com/" target="_blank">Maxime Laurin</a></span></div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center mt-3">
+                    <a class="btn btn-primary" href="../contactde8d.html?de=https%3A%2F%2Fapical.xyz%2Fformations%2Fwordpress">Écrivez-moi&nbsp;!</a>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </footer>
+
+    
 
 
 
 
 
+</body>
 </html>
+
