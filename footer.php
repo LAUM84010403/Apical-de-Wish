a6104926c6592c8d44e0695eb6bcef9bf18f1319
Max12243 <laurin.maxime1@gmail.com>
2023-10-18 15:18:28 -0400

    Avancement

---
+++ footer.php
@@ -0,0 +1,37 @@
+<div class="boutonsbas">
+                    </div>
+    </div>
+
+            </div>
+            
+            <div class="push"></div>
+        </div>
+
+        <div class="publicite baspage">
+            <div class="pubhebergement">
+                <p>Site fièrement hébergé chez <a href="http://www.a2hosting.com/?aid=5ca65a17be949" target="_top">A2 Hosting</a>.</p>
+                <p><a href="https://www.a2hosting.com/?aid=5ca65a17be949&amp;bid=ed1c4a67" target="_top"><img src="../../affiliates.a2hosting.com/accounts/default1/banners/ed1c4a67.jpg" alt="" title="" width="728" height="90" /></a><img style="border:0" src="https://affiliates.a2hosting.com/scripts/imp.php?aid=5ca65a17be949&amp;bid=ed1c4a67" width="1" height="1" alt="" /></p>
+            </div>
+        </div>
+    </div>
+
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">© 2023 Tous droits réservés.</div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">Conception et développement de la plateforme : <span class="auteur"><a href="https://christianelagace.com/" target="_blank">Christiane Lagacé</a></span></div>
+                <div class="col text-center">En vue du Cours Dev Web 2, recopiage du site : <span class="auteur"><a href="https://christianelagace.com/" target="_blank">Maxime Laurin</a></span></div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center mt-3">
+                    <a class="btn btn-primary" href="../contactde8d.html?de=https%3A%2F%2Fapical.xyz%2Fformations%2Fwordpress">Écrivez-moi&nbsp;!</a>
+                </div>
+            </div>
+        </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="fr-CA">
+
+
+<!-- Mirrored from apical.xyz/formations/wordpress by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 09 Oct 2023 20:47:34 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5HQFPLZYQK"></script>
+
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Contenu de la formation  PUB020&amp;nbsp;: WordPress sur Apical." />
+    <meta name="author" content="Christiane Lagacé : https://christianelagace.com">
+    
+    <meta name="csrf-token" id="csrf-token" content="uzdpbgMctuI7uF4ieyWMTXrX8CuXoGiKvBdeuUSN">
+
+    <title>Apical | Formation PUB020 : WordPress</title>
+    <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+
+    
+    
+             
+        
+                    <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
+            <link rel="manifest" href="../site.html">
+            <link rel="mask-icon" href="../safari-pinned-tab.svg" color="#3543a0">
+            <meta name="msapplication-TileColor" content="#2b5797">
+            <meta name="theme-color" content="#ffffff">
+            
+    <meta name="robots" content="noodp" />
+
+    <script data-ad-client="ca-pub-0968524214519104" async src="../../pagead2.googlesyndication.com/pagead/js/f.txt"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+    
+    <link href="https://fonts.googleapis.com/css?family=Homemade+Apple" rel="stylesheet">
+    
+</head>
+
+
+
+
+<header id="messtyles">
+<div class="aucunepubauto">
+        <div class="logo fix-menu" id="zoneentete">
+            <div class="container">
+                <div class="avant"></div>
+                    <div class="apres">
+                        <div id="menuicones" class="porteuroffset fix-menu" data-verticaloffset="47" data-horizontaloffset="0">
+                            <a href=""><img src="https://apical.xyz/medias/commun/Accueil-MenuSecondaire.svg" alt="Accueil" title="Accueil"></a>
+                            <a href="#" class="ouvrirpopupchristiane ouvrirpopuprecherche" data-target="#popuprecherche"><img src="https://apical.xyz/medias/commun/Rechercher-MenuSecondaire.svg" alt="Recherche" title="Rechercher" onclick="ouvrirDiv(this)"></a>
+                            <a href="#" class="ouvrirpopupchristiane ouvrirpopupauthentification" data-target="#popupauthentification"><img src="https://apical.xyz/medias/commun/Login-MenuSecondaire.svg" alt="Authentification" title></a>
+                        </div>
+
+
+                    </div>
+                    <div class="centre">
+                        <img src="https://apical.xyz/medias/fr/LogoApical-blanc.svg" alt="Apical, ma plateforme d'apprentissage">
+                    </div>
+                </div>
+            </div>
+
+
+            <script>
+        function ouvrirDiv(icon) {
+
+            var divOuverte = document.querySelector('.div-ouverte');
+    
+    if (divOuverte.style.display === 'block') {
+        divOuverte.style.display = 'none'; // Masque la div si elle est déjà affichée
+    } else {
+        divOuverte.style.display = 'block'; // Affiche la div si elle est masquée
+    }
+        }
+    </script>
+
+        
+            
